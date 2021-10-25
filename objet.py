a2214d2b870c6cd6934703254846fdf4c15ec6fa
PatBoud <patboud@MacBook-Pro-de-PatBoud.local>
2021-10-24 21:10:19 -0400

    Changement mineur de l'entête

--- objet.py
+++ objet.py
@@ -11,14 +11,15 @@ rat2 = mammifere("rat", "Kora", 4, "koui koui!")
 gecko = reptile("gecko", "Paul", 4, False)
 serpent = reptile("serpent", "Black", 0, True)
 
-print ("========================================")
-print ("")
-
 # Création d'une liste d'animaux contenant tous les
 # objets animaux créés précédemment.
 # Ceci facilitera l'utilisation d'une boucle pour afficher 
 # leurs informations.
 animaux = [chien1, chien2, chat, araignee, rat1, rat2, gecko, serpent]
+
+
+print ("========================================")
+print ("")
 
 # Boucle dans la liste d'animaux
 for anim in animaux:
