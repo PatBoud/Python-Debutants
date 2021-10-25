43fa4b04cc10789cbfc76fce4911f63b2009b3f5
Patrice Boudreault <patriceboudreault@cegepjonquiere.ca>
2021-10-25 12:20:20 -0400

    Ajout de commentaires, délais à l'affichage

--- nature.py
+++ nature.py
@@ -3,6 +3,7 @@
 # Il ne fait rien comme tel.
 
 class animal():
+  # Constructeur
   def __init__(self, espece, nom):
     self.espece = espece
     self.nom = nom
@@ -26,18 +27,29 @@ class animal():
 
 # Classe mammifere, qui hérite de la classe animal
 class mammifere(animal):
+  # Constructeur
   def __init__(self, espece, nom, pattes, son):
+    # Appel du constructeur de la classe parent
     super().__init__(espece, nom)
+
+    # Définition des variables privées à partir des valeurs
+    # reçues en paramètres au constructeur
     self.pattes = pattes
     self.son = son
 
   def sonne(self):
     print(self.son)
 
+
 # Classe reptile, qui hérite de la classe animal
 class reptile(animal):
+  # Constructeur
   def __init__(self, espece, nom, pattes, veneneux):
+    # Appel du constructeur de la classe parent
     super().__init__(espece, nom)
+
+    # Définition des variables privées à partir des valeurs
+    # reçues en paramètres au constructeur
     self.pattes = pattes
     self.veneneux = veneneux
 
@@ -47,13 +59,20 @@ class reptile(animal):
     else:
       return ("")
 
+
 # Classe arachnide, qui hérite de la classe animal
 class arachnide(animal):
   def __init__(self, espece, nom, pattes, veneneux):
+    # Appel du constructeur de la classe parent
     super().__init__(espece, nom)
+
+    # Définition des variables privées à partir des valeurs
+    # reçues en paramètres au constructeur
     self.pattes = pattes
     self.veneneux = veneneux
 
   def getDetails(self):
     if (self.veneneux):
-      return "Attention! Vénéneux!"
+      return "ATTENTION! Vénéneux!"
+    else:
+      return ("")
--- objet.py
+++ objet.py
@@ -1,29 +1,47 @@
 # Importation du module de classes contenant les classes d'animaux
 from nature import *
 
+# Importation de la méthode time.sleep(), pour ralentir un peu l'exécution
+# en ajoutant des délais pendant l'affichage de la liste
+from time import sleep
+
 # Création d'objets à partir des classes d'animaux
 chien1 = mammifere("chien", "Billie", 4, "ouaf!")
 chien2 = mammifere("chien", "Rex", 4, "ouaf!")
 chat = mammifere("chat", "Rubis", 3, "miaaaaaaaou")
-araignee = arachnide("araignée", "LaMort", 8, True)
 rat1 = mammifere("rat", "Shaki", 4, "koui koui!")
 rat2 = mammifere("rat", "Kora", 4, "koui koui!")
+araignee = arachnide("araignée", "LaMort", 8, True)
 gecko = reptile("gecko", "Paul", 4, False)
+lezard = reptile("lézard", "Bob", 4, False)
 serpent = reptile("serpent", "Black", 0, True)
 
 # Création d'une liste d'animaux contenant tous les
 # objets animaux créés précédemment.
 # Ceci facilitera l'utilisation d'une boucle pour afficher 
 # leurs informations.
-animaux = [chien1, chien2, chat, araignee, rat1, rat2, gecko, serpent]
+animaux = [chien1, chien2, lezard, chat, araignee, rat1, rat2, gecko, serpent]
 
-
+print ("")
+print ("========================================")
+print ("            LISTE D'ANIMAUX             ")
 print ("========================================")
 print ("")
 
-# Boucle dans la liste d'animaux
+# Boucle dans la liste des objets animaux pour afficher leurs informations.
+# Dans chaque cycle de la boucle, "anim" représente l'élément actuel dans la liste.
+# On obtient les informations de chacun en invoquant les méthodes .getNom(), getEspece() et getPattes() .
 for anim in animaux:
   print (anim.getNom() + ", " + anim.getEspece() + ", " + str(anim.getPattes()) + " pattes")
   anim.sonne()
-  print (anim.getDetails())
-  print ("---------------------")
+  if (anim.getDetails() != ""):
+    print (anim.getDetails())
+  print ("-------------------------------")
+  # Délais de 0.25 seconde)
+  sleep(0.25)
+
+print ("")
+print ("========================================")
+print ("            Fin de la liste             ")
+print ("========================================")
+print ("")
